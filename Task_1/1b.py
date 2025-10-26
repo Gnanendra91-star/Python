c8f47cffd43f5190ea7b8a7df1dc4f7877a03b56
Gnanendra91-star <ngnanendra91@gmail.com>
2025-10-26 14:08:19 +0530

    Create 1b.py

---
+++ Task_1/1b.py
@@ -0,0 +1,7 @@
+cls = float(input(‘Enter temperature in Celsius: ‘))  
+fah = (cls * 1.8) + 32  
+print(‘%0.1f Celsius is equal to %0.1f degrees Fahrenheit’%(cls,fah)) 
+
+output:
+Enter temperature in Celsius: 50
+50.0 Celsius is equal to 122.0 degrees Fahrenheit
