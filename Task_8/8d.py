0059913a9b4f5b8011c738b77fb91892fd309729
Gnanendra91-star <ngnanendra91@gmail.com>
2025-10-26 22:07:20 +0530

    Create 8d.py

---
+++ Task_8/8d.py
@@ -0,0 +1,28 @@
+def uppercase_decorator(func):
+    def wrapper(text):
+        return func(text).upper()
+    return wrapper
+
+def lowercase_decorator(func):
+    def wrapper(text):
+        return func(text).lower()
+    return wrapper
+
+@uppercase_decorator
+def shout(text):
+    return text
+
+@lowercase_decorator
+def whisper(text):
+    return text
+
+def greet(func):
+    greeting = func("Hi, I am created by a function passed as an argument.")
+    print(greeting)
+
+greet(shout)
+greet(whisper)
+
+output:
+HI, I AM CREATED BY A FUNCTION PASSED AS AN ARGUMENT.
+hi, i am created by a function passed as an argument.
